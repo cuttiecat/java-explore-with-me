07ce525d89254176ecf4f1d7e4f7c1017634d398
cuttiecat <godrdinasvetlana@yandex.ru>
2023-09-30 12:48:55 +0400

    First Commit. Second try. (c) hallelujahS

--- stats-service/stats-server/src/main/java/ru/practicum/stats_server/mapper/StatsMapperImpl.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats_server/mapper/StatsMapperImpl.java
@@ -1,14 +1,14 @@
-package ru.practicum.stats_server.mapper;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import ru.practicum.stats_common.model.EndpointHit;
-import ru.practicum.stats_server.model.Stats;
-
-import java.time.LocalDateTime;
-
-@Mapper(componentModel = "spring")
-public interface StatsMapperImpl {
-    @Mapping(target = "timestamp", expression = "java(timestamp)")
-    Stats toStats(EndpointHit endpointHit, LocalDateTime timestamp);
-}
+//package ru.practicum.stats_server.mapper;
+//
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//import ru.practicum.stats_common.model.EndpointHit;
+//import ru.practicum.stats_server.model.Stats;
+//
+//import java.time.LocalDateTime;
+//
+//@Mapper(componentModel = "spring")
+//public interface StatsMapperImpl {
+//    @Mapping(target = "timestamp", expression = "java(timestamp)")
+//    Stats toStats(EndpointHit endpointHit, LocalDateTime timestamp);
+//}
